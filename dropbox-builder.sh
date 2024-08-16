d1c9ab28054d9c90d5be523d227aaa796fc9f2cc
iVAN <88724353+ivan-hc@users.noreply.github.com>
2024-08-16 22:09:57 +0200

    Update dropbox-builder.sh

--- dropbox-builder.sh
+++ dropbox-builder.sh
@@ -3,7 +3,7 @@
 APP=dropbox
 mkdir tmp
 cd ./tmp
-wget -q "$(wget -q https://api.github.com/repos/probonopd/go-appimage/releases -O - | sed 's/"/ /g; s/ /\n/g' | grep -o 'https.*continuous.*tool.*86_64.*mage$')" -O appimagetool
+wget -q https://github.com/AppImage/appimagetool/releases/download/continuous/appimagetool-x86_64.AppImage -O appimagetool
 chmod a+x ./appimagetool
 
 DL=$(echo "https://aur.andontie.net/x86_64/$(curl -Ls https://aur.andontie.net/x86_64/ | grep dropbox | head -1 | grep -o -P '(?<=href=").*(?=">dropbox)')")
@@ -26,6 +26,6 @@ exec "${HERE}"/opt/dropbox/dropbox "$@"
 EOF
 chmod a+x ./$APP.AppDir/AppRun
 
-ARCH=x86_64 VERSION=$(./appimagetool -v | grep -o '[[:digit:]]*') ./appimagetool -s ./$APP.AppDir
+ARCH=x86_64 ./appimagetool --comp zstd --mksquashfs-opt -Xcompression-level --mksquashfs-opt 20 ./$APP.AppDir
 cd ..
 mv ./tmp/*.AppImage ./Dropbox-$VERSION-x86_64.AppImage
